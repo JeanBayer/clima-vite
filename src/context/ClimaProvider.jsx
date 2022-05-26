7a4497cec2a17e5e0a2ca24bc671e12b4b32100c
Jhan Bayer <jhanbayer@gmail.com>
2022-05-26 13:23:21 -0400

    feat: add context and hook

---
+++ src/context/ClimaProvider.jsx
@@ -0,0 +1,32 @@
+import { useState, createContext } from "react";
+
+const ClimaContext = createContext();
+
+const ClimaProvider = ({ children }) => {
+  const [busqueda, setBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const datosBusqueda = (e) => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <ClimaContext.Provider
+      value={{
+        busqueda,
+        datosBusqueda,
+      }}
+    >
+      {children}
+    </ClimaContext.Provider>
+  );
+};
+
+export { ClimaProvider };
+
+export default ClimaContext;
