8b7e343e0a99a5b3ee8498a95e0d4bae09b8f546
Jhan Bayer <jhanbayer@gmail.com>
2022-05-26 16:54:29 -0400

    feat: display data in Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -3,10 +3,25 @@ import useClima from "../hooks/useClima";
 const Resultado = () => {
   const { resultado } = useClima();
   console.log({ resultado });
-  const { name } = resultado;
+  const { name, main } = resultado;
+
+  const toCelsius = (kelvin) => {
+    return Math.round(kelvin - 273.15);
+  };
   return (
-    <div className="contenedor">
+    <div className="contenedor clima">
       <h2>El clima de {name} es:</h2>
+      <p>
+        {toCelsius(main.temp)} <span>&#x2103;</span>
+      </p>
+      <div className="temp_min_max">
+        <p>
+          Minima: {toCelsius(main.temp_min)} <span>&#x2103;</span>
+        </p>
+        <p>
+          Maxima: {toCelsius(main.temp_max)} <span>&#x2103;</span>
+        </p>
+      </div>
     </div>
   );
 };
